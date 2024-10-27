13d7c375d7880dee707c94eb5cee556978e12255
Murs000 <129412232+Murs000@users.noreply.github.com>
2024-10-27 12:52:51 +0400

    App. Centre Header text.

--- RealTimeAdminPanel/obj/Debug/net8.0/RealTimeAdminPanel.AssemblyInfo.cs
+++ RealTimeAdminPanel/obj/Debug/net8.0/RealTimeAdminPanel.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RealTimeAdminPanel")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+705d0942c233fcad2c92b3086ca23981fe1a9bf1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1069cb0576d9cb386efee057b16d23900d035b4")]
 [assembly: System.Reflection.AssemblyProductAttribute("RealTimeAdminPanel")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RealTimeAdminPanel")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
